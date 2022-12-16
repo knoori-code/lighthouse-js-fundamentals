e799c5baadd08591579c9b3bba220849dc6978a3
knoori-code <knoori3@gmail.com>
2022-12-15 20:37:01 -0500

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -9,3 +9,22 @@ if (temperature < 0) {
 }
 
 console.log('Now you\'re ready to go outside!');
+
+
+const isCitizen = true;
+const age = 26;
+
+
+if (isCitizen && age > 18) {
+    console.log('You are elgible to vote');
+}
+
+
+if (temperature < -40 || temperature > 40) {
+    console.log('Maybe going outside isn\'t such a great idea..');
+}
+
+
+if (!raining) {
+    console.log('Leave your umbrealla at home!');
+}

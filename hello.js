db5e0c8d25ddfcd24f851e1617dc827ea5a865d6
knoori-code <knoori3@gmail.com>
2022-12-17 18:00:26 -0500

    more examples added

--- hello.js
+++ hello.js
@@ -5,3 +5,18 @@ const sayHello = function(name) {
 sayHello('Khalid')
 sayHello('Ibby')
 sayHello('Aaisha')
+
+// Using console.log() to print message
+const sayHelloToConsole = function(name) {
+    console.log('Hello, ' + name);
+}
+sayHelloToConsole('John');
+
+
+// Using return to return a value
+const returnSayHello = function(name) {
+    return "Hello, " + name;
+}
+const greeting = returnSayHello('John')
+console.log(greeting);
+// The value returned can be stored in a variable to be used later. Can use greeting in console.log to display message

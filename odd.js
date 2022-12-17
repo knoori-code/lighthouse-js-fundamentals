6f712402fd12b83dfb11e6419256b844b96c3b16
knoori-code <knoori3@gmail.com>
2022-12-17 18:23:06 -0500

    Program to determine if number is odd or not

---
+++ odd.js
@@ -0,0 +1,14 @@
+let isOdd = function(number) {
+    return number % 2 !== 0
+}
+
+console.log("1 is odd: " + isOdd(1));
+console.log("2 is odd: " + isOdd(2));
+console.log("3 is odd: " + isOdd(3));
+console.log("4 is odd: " + isOdd(4));
+console.log("5 is odd: " + isOdd(5));
+console.log("6 is odd: " + isOdd(6));
+console.log("7 is odd: " + isOdd(7));
+console.log("8 is odd: " + isOdd(8));
+console.log("9 is odd: " + isOdd(9));
+console.log("10 is odd: " + isOdd(10));

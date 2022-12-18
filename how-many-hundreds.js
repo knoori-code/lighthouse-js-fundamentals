9217a2a4e9caaaa43fbf81d7c031fccd58f3e2ac
knoori-code <knoori3@gmail.com>
2022-12-17 22:08:35 -0500

    How many hundreds assignment

---
+++ how-many-hundreds.js
@@ -0,0 +1,10 @@
+const howManyHundreds = function(number) {
+  let hundreds = number / 100 
+  hundreds = Math.floor(hundreds)
+  return hundreds
+}
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);

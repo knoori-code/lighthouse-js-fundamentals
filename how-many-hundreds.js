b91ba343b7a19fc6a6df31fde3d8b21d712e5a0f
knoori-code <knoori3@gmail.com>
2022-12-18 17:47:45 -0500

    area calculator assignment

--- how-many-hundreds.js
+++ how-many-hundreds.js
@@ -8,3 +8,4 @@ console.log(howManyHundreds(894), "=?", 8);
 console.log(howManyHundreds(520), "=?", 5);
 console.log(howManyHundreds(99), "=?", 0);
 console.log(howManyHundreds(0), "=?", 0);
+jkjfdkakdjjafkladsf

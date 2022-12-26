6191a9d83388456bb907fb8a690f933dde4f2717
knoori-code <knoori3@gmail.com>
2022-12-26 09:21:07 -0500

    Program which sums the two largest numbers in an array

---
+++ sum-largest-numbers.js
@@ -0,0 +1,18 @@
+const sumLargestNumbers = function(data) {
+  let highNum = 0
+  let secondHighNum = 0
+  for (let num of data) {
+    if (num > highNum) {
+      highNum = num;
+    }
+  }
+  for (let num of data) {
+    if (num > secondHighNum && num !== highNum) {
+      secondHighNum = num;
+    }
+  }
+  return highNum + secondHighNum
+}
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));

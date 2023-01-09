d83f35bdd3ea45d37f6f0f42e99f4fc0c6901726
knoori-code <knoori3@gmail.com>
2023-01-08 20:40:03 -0500

    initial commit

---
+++ multiplication-table.js
@@ -0,0 +1,30 @@
+const multiplicationTable = function (maxValue) {
+  let table = []
+  for (let i = 0; i < maxValue; i++) {
+    // Create arrays inside initial array
+    table.push([])
+  }
+  // Create top row of array
+  for (let j = 1; j <= maxValue; j++) {
+    table[0].push(j)
+  }
+  // Fill in first number of each row
+  for (i = 1; i < maxValue; i++) {
+    table[i].push(i + 1)
+  }
+  for (i = 1; i < maxValue; i++) {
+    row = table[i];
+    for (j = 1; j < maxValue; j++) {
+      row.push(table[0][j] * row[0])
+    }
+  }
+  let stringVersion = ''
+  for (i = 0; i < maxValue; i++) {
+    stringVersion += table[i].join(' ') + '\n'
+  }
+  return stringVersion
+}
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
